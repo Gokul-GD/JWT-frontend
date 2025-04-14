3a4f72d37ff64624882b098a875f53627e1cc7cc
Gokul-GD <gokuld150424@gmail.com>
2025-04-14 12:18:41 +0530

    chore: updated Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
@@ -11,6 +11,7 @@ function App() {
   
 
   return (
+    <Router>
     <Routes>
       <Route path="/" element={<Register />} />
       <Route path="/login" element={<Login />} />
@@ -21,6 +22,7 @@ function App() {
         } />
         <Route path="/forgot-password" element={<ForgotPassword />} />
     </Routes>
+    </Router>
     
   );
 }
